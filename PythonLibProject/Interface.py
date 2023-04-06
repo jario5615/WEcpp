bcd5dc20d5ca7a728740009eb727a647b6d3aa07
David Kendell <david.kendell@hotmail.co.uk>
2023-04-06 11:11:57 +0100

    Fixed problems in Interface.py

--- PythonLibProject/Interface.py
+++ PythonLibProject/Interface.py
@@ -1,5 +1,6 @@
 from Controller import *
 from prettytable import PrettyTable
+from datetime import date
 
 def printSearch(search):
     t = PrettyTable(["D", "Title", "Author ", " Genre"])
@@ -12,7 +13,7 @@ def printSynop(search):
     for a in search:
         t.add_row(a)
     print(t)
-control = Controller()
+control = Controller("LibraryDatabaseProd.sql")
 
 choice = ""
 
@@ -56,18 +57,38 @@ while choice != "0":
                 print("2 - Delete Author")
                 print("3 - Add Book")
                 print("4 - Delete Book")
+                print("5 - Add Genre")
+                print("6 - Delete Genre")
                 subchoice = input("Enter Enter number of relevant operation: ")
                 match subchoice:
                     case "1":
                         id = int(input("Enter author id: "))
+                        while id == "":
+                            id = int(input("Enter author id: "))
                         firstname = input("Enter first name:")
+                        while firstname == "":
+                            firstname = input("Enter first name:")
                         middlename = input("Enter middle name (leave blank for null): ")
                         if middlename == "": middlename = None
                         lastname = input("Enter last name:")
+                        while lastname == "":
+                            lastname = input("Enter last name:")
                         gender = input("Enter Gender M/F: ")
                         if gender == "": gender = None
                         dob = input("Enter date of birth in format YYYY-MM-DD: ")
+                        while True:
+                            try:
+                                date.fromisoformat(dob)
+                                break
+                            except Exception:
+                                dob = input("Enter date of birth in format YYYY-MM-DD: ")
                         dod = input("Enter date of death in format YYYY-MM-DD: ")
+                        while dod != "":
+                            try:
+                                date.fromisoformat(dod)
+                                break
+                            except Exception:
+                                dod = input("Enter date of birth in format YYYY-MM-DD: ")
                         if dob == "": dob = None
                         insert = {"id": id, "firstname": firstname, "middlename": middlename, "lastname": lastname, "gender": gender, "dateofbirth": dob, "dateofdeath": dod}
                         control.addThing("author", insert)
@@ -77,15 +98,45 @@ while choice != "0":
                         control.deleteThing("author", id)
                     case "3":
                         name = input("Enter name of book to add")
+                        while name == "":
+                            name = input("Enter name of book to add")
                         bookid = input("Enter the book ID")
+                        while bookid == "":
+                            bookid = input("Enter the book ID")
                         authorid = input("Enter the author ID")
+                        while authorid == "":
+                            authorid = input("Enter the author ID")
                         genreid = input("Enter the genre ID")
+                        while genreid == "":
+                            genreid = input("Enter the genre ID")
                         synop = input("Enter book synopsis")
                         pubdate = input("Enter publication date in format YYYY-MM-DD")
+                        while pubdate != "":
+                            try:
+                                date.fromisoformat(pubdate)
+                                break
+                            except Exception:
+                                pubdate = input("Enter publication date in format YYYY-MM-DD: ")
                         insert = {"bookid" : bookid, "authorid": authorid, "genreid": genreid, "title": name, "synopsis": synop, "publicationdate": pubdate}
-                        control.addThing("book", insert)
+                        message = str(control.addThing("book", insert))
+                        if message.find("genre") != -1:
+                            print("Book must have a valid genre id")
+                        if message.find("author") != -1:
+                            print("Book must have a valid author id")
 
 
                     case "4":
-                        id = input("Enter ID of author to delete")
-                        control.deleteThing("book", id)
+                        authorid = input("Enter ID of author to delete")
+                        control.deleteThing("book", authorid)
+
+                    case "5":
+                        genreid = input("Enter genreid")
+                        while genreid == "":
+                            genreid = input("Enter genreid:")
+                        genrename = input("Enter genre name")
+                        while genreid == "":
+                            genrename = input("Enter genre name:")
+
+                    case "6":
+                        genreid = input("Enter ID of genre to delete")
+                        control.deleteThing("genre", genreid)
