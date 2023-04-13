2238850588000875abfb03b40dd79dc573c76ffe
Sandford Harris <sandfordharris01@gmail.com>
2023-04-13 16:09:25 +0100

    made inputs into command line arguments

--- BashBackup/SimpleBackup.sh
+++ BashBackup/SimpleBackup.sh
@@ -1,15 +1,26 @@
 #! /bin/bash
-
-read -p "Enter path to source from " source
-read -p "Enter destination path " destination
-if [[ ! -d $source ]]
-then
-	echo "WARN: Source directory does not exist"
-else
-	if [[ ! -d $destination ]]
-	then
-		mkdir $destination
-	fi
-	tar cfz "${destination}/backup.tar.gz" $source
-	echo "Backup created successfully"
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 [source] [destination]"
+    exit 1
 fi
+
+source=$1
+destination=$2
+
+if [[ ! -d $source ]]; then
+    echo "ERROR: Source directory does not exist"
+    exit 1
+fi
+
+if [[ ! -d $destination ]]; then
+    read -p "$destination isn't an existing directory. Create new directory here? (y/n) " ans
+    if [[ $ans -ne "y" ]]; then
+        exit 1
+	fi
+fi
+#create directory with current date and time of backup
+destination="$destination/$(date +%Y-%m-%d_%H:%M)"
+mkdir -p $destination
+
+tar cfz "${destination}/backup.tar.gz" $source
+echo "Backup created successfully"
